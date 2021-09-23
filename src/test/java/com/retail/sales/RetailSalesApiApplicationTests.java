31a4ef165bc4b3a9a62be920cb47c0ef00e65937
YamunaKrishna01 <yamunakrishna1920@gmail.com>
2021-09-23 17:06:40 +0530

    testcases added

--- src/test/java/com/retail/sales/RetailSalesApiApplicationTests.java
+++ src/test/java/com/retail/sales/RetailSalesApiApplicationTests.java
@@ -1,13 +1,25 @@
 package com.retail.sales;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.retail.sales.service.RetailsService;
+
 @SpringBootTest
 class RetailSalesApiApplicationTests {
+	RetailsService service = new RetailsService();
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getNetPayable() {
+		double pay = service.getNetPayableAmount("employee", 3500, "non_grocery");
+		assertEquals(1000, pay);
+
 	}
 
 }
